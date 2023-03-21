817ed5e8ba767f26dbafa3d5d19f6e4b27b68fd4
Abhishek1923 <abhishek230401@gmail.com>
2023-03-21 22:21:53 +0530

    Day 1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,14 @@
 print("Starting Python, Today!!")
+
+list=[[1,23,4],2,1,4,67,4]
+# list.remove(list[0][1])
+# list.remove[0][1]
+
+print(list)
+
+# list=[23,4,654,1,25,6]
+
+# if tuple holds mutable objects the can be changed
+tup1=(23,'time',[4,5,6],"scissors")
+tup1[2][2]=11
+print(tup1)
